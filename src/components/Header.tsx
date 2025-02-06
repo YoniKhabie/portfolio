1c57cc89852159ab08db8b1911e9249ab2a89730
YoniIfrah <yonifrah996@gmail.com>
2025-02-06 18:33:16 +0200

    pixel perfect fix #2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const prompt =
 export default function Header() {
     return (
         <Section id="about">
-            <div className="mx-auto max-w-7xl px-6 ml-6.75 lg:px-8">
+            <div className="mx-auto max-w-7xl px-6 ml-6.75 sm:ml-1 lg:px-8">
                 <div className="mx-auto max-w-2xl lg:mx-0">
                     <h1 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl fade-in-from-top">
                         Yoni Ifrah
@@ -18,10 +18,10 @@ export default function Header() {
                 </div>
                 <TypingEffect text={prompt} />
             </div>
-            <div className="mx-auto max-w-7xl px-6 lg:px-8 ml-7">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <ul
                     role="list"
-                    className="mt-30 flex flex-wrap justify-start gap-x-1 md:mt-10 sm:gap-y-4:mt-10 xl:col-span-2 "
+                    className="justify-center mt-30 flex flex-wrap sm:justify-start gap-x-1 md:mt-10 sm:gap-y-4:mt-10 xl:col-span-2 "
                 >
                     {headerData.map((image, index) => (
                         <li key={index} className="fade-in-from-bottom pt-2">
